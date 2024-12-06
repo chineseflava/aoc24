a780606f9f6eb30dce4f0189bc533a0dca13b9e7
Björn Walter <bjornwa@axis.com>
2024-12-06 11:43:00 +0100

    Day6 part 2
     - Very ineffective brute force solution, should implement something
       like keeping track of direction the guard went on previous tiles so
       the "stuck-detection" becomes more effective...

--- 6.py
+++ 6.py
@@ -1,10 +1,11 @@
-import numpy as np
+import time
 
 class Guard:
     def __init__(self, pos, direction):
         self.pos = pos
         self.direction = direction
         self.init_pos = pos
+        self.init_dir = direction
         self.outside = False
     def rotate_right(self):
         if self.direction == "^":
@@ -62,32 +63,6 @@ class GuardMap:
         else:
             self.guard.set_pos(next_pos)
 
-    def step_backward(self):
-        pos = self.guard.pos
-        direction = self.guard.direction
-        self.matrix[pos[0]][pos[1]] = 1
-        #Check position in front of guard
-        next_pos = ()
-        if direction == "^":
-            next_pos = (pos[0]+1, pos[1])
-        if direction == ">":
-            next_pos = (pos[0], pos[1]-1)
-        if direction == "v":
-            next_pos = (pos[0]-1, pos[1])
-        if direction == "<":
-            next_pos = (pos[0], pos[1]+1)
-        #If obstacles or edge, rotate
-        n = len(self.matrix)
-        #If obstacles or outside matrix, rotate
-        if next_pos in self.obstacles:
-            self.guard.rotate_left()
-        # If next_pos is outside of matrix, guard is out.
-        elif next_pos[0]>=n or next_pos[1]>=n or next_pos[0]*next_pos[1]<0:
-            self.guard.outside = True
-        #if not, move guard to this position
-        else:
-            self.guard.set_pos(next_pos)
-
     def get_score(self):
         score = 0
         for item in self.matrix:
@@ -115,7 +90,7 @@ def part1():
     data = []
     for line in lines:
         data.append(line.strip("\n"))
-    print(data)
+    #print(data)
 
     matrix, obstacles, guard = handle_data(data)
     guard_map = GuardMap(matrix, obstacles, guard)
@@ -123,12 +98,58 @@ def part1():
 
     while not guard_map.guard.outside:
         guard_map.step_forward()
-        print(guard_map.guard.pos)
+        #print(guard_map.guard.pos)
     
     score = guard_map.get_score()
 
     return score
 
+def part2():
+    with open("./input/6", 'r') as f:
+        lines = f.readlines()
+    data = []
+    for line in lines:
+        data.append(line.strip("\n"))
+    #print(data)
+
+    matrix, obstacles, guard = handle_data(data)
+    guard_map = GuardMap(matrix, obstacles, guard)
+
+    n_stuck = 0
+    max_steps = 8000
+    for y, row in enumerate(guard_map.matrix):
+        for x, num in enumerate(row):
+            guard_map.guard.pos = guard_map.guard.init_pos
+            guard_map.guard.direction = guard_map.guard.init_dir
+            guard_map.guard.outside = False
+            obstacle = (y,x)
+            #print(f"Testing {obstacle=}")
+            if obstacle in guard_map.obstacles:
+                continue
+            guard_map.obstacles.add(obstacle)
+            steps_taken = 0
+            while not guard_map.guard.outside:
+                guard_map.step_forward()
+                steps_taken += 1
+                #print(steps_taken)
+                if steps_taken >= max_steps:
+                    n_stuck += 1
+                    #print(f"{n_stuck=}")
+                    guard_map.obstacles.remove(obstacle)
+                    break
+            try:
+                guard_map.obstacles.remove(obstacle)
+            except:
+                continue
+                #print(guard_map.guard.pos)
+    
+    score = n_stuck
+
+    return score
+
 if __name__ == "__main__":
-    print(f"Answer part 1: {part1()}")
-    #print(f"Answer part 2: {part2()}")
+    #print(f"Answer part 1: {part1()}")
+    start = time.time()
+    print(f"Answer part 2: {part2()}")
+    end = time.time()
+    print(end-start)
